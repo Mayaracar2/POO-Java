812e0c7896c1ca9c4ac461e1df7abe18a2cb5ecd
mayaracar2 <mayara.c@ges.inatel.br>
2026-03-04 22:34:21 -0300

    Atualizado

--- src/org/Ex5/cinema2.java
+++
@@ -1,7 +0,0 @@
-package org.Ex5;
-
-public class cinema2 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/org/aula4/Aula4_exemplos.java
@@ -0,0 +1,51 @@
+package org.aula4;
+
+import java.util.Scanner;
+
+public class Aula4_exemplos {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int qtd = teclado.nextInt();
+
+       /* while(qtd > 0){
+            System.out.println(qtd);
+            qtd = qtd--;
+        }*/
+
+        /*do {
+            System.out.println(qtd);
+            qtd = qtd - 1;
+        } while (qtd > 0);
+
+        for (int i=0; i<10; i++){
+            System.out.println(i);
+        }*/
+
+        /*for(int i=qtd; i>0; i--){
+            System.out.println(i);
+        }*/
+
+        /*int[] vetor = {1,3,6,2};
+
+        for(int numero: vetor){
+            System.out.println(numero);
+        }
+
+        System.out.println("-------------");
+
+        for(int i=0; i<vetor.length; i++){
+            System.out.println(vetor[i]);
+        }*/
+
+       /* while (qtd >0){
+            System.out.println(qtd);
+            qtd--;
+
+            if(qtd==5)
+                break; //para quebrar o loop
+        }*/
+
+
+    }
+}
